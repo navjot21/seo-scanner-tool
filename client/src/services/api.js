2b57b30cdffeff7b2b3bbe9f6b5817c07cd25ca1
Navjot Singh Narula <navjot.narula@tweaking.local>
2026-05-29 15:25:41 +0530

    Added live backend API

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://seo-scanner-tool.onrender.com/api",
 });
 
 export default API;
